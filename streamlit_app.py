295b1679ea819f8b1288562d172fcde3cdf310c4
zvinrose <72659184+zvinrose@users.noreply.github.com>
2022-11-05 21:07:04 +0100

    Update streamlit_app.py
    Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.header("HThe fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
