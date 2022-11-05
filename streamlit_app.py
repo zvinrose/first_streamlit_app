f8aa7e14a68c1bbac1d9ab1b2332e4eb9ffbc4a4
zvinrose <72659184+zvinrose@users.noreply.github.com>
2022-11-05 20:14:42 +0100

    Update streamlit_app.py
    removing raw fuityvice json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # get the json from fruityvice
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
